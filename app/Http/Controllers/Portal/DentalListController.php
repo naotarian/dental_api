45191e604e2f40ce81ff98bc27e521dd946111c1
pg002 <pg.002bbb@gmail.com>
2023-06-08 20:30:59 +0900

    カレンダー作成

--- app/Http/Controllers/Portal/DentalListController.php
+++ app/Http/Controllers/Portal/DentalListController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Region;
 use App\Models\Manage;
+use Carbon\Carbon;
+use Carbon\CarbonPeriod;
 
 class DentalListController extends Controller
 {
@@ -21,7 +23,28 @@ class DentalListController extends Controller
     {
         $id = $request['id'];
         $dental = Manage::where('id', $id)->with('selected_station')->with('treatments')->first();
-        $contents = ['dental' => $dental];
+        //カレンダーの日付作成
+        //当月の初日を取得
+        $target_first = Carbon::now()->startOfMonth();
+        //月曜日から計算した曜日の差分を取得
+        $w = $target_first->copy()->dayOfWeekIso - 1;
+        $cal_start = $target_first->copy()->subDays($w)->toDateString();
+
+        //当月の最終日を取得
+        $target_last = Carbon::now()->endOfMonth();
+
+        //月曜日から計算した曜日の差分を取得
+        $w = 7 - $target_last->copy()->dayOfWeekIso;
+        $cal_end = $target_last->copy()->addDays($w)->toDateString();
+        $dates = CarbonPeriod::create($cal_start, $cal_end)->toArray();
+        $dates_format = [];
+        foreach ($dates as $date) {
+            $dates_format[$date->format('Y/m/d')]['date'] = $date->format('n/j');
+            $dates_format[$date->format('Y/m/d')]['dow'] = $date->dayOfWeekIso;
+        }
+        $split_dates = array_chunk($dates_format, 7);
+        \Log::info($dates);
+        $contents = ['dental' => $dental, 'dates' => $split_dates];
         return response()->json($contents);
     }
 }
