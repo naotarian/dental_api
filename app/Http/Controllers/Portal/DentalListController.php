a50793baa659c31b2243be8de17e2e3cf29122ad
pg002 <pg.002bbb@gmail.com>
2023-06-15 20:39:37 +0900

    wip

--- app/Http/Controllers/Portal/DentalListController.php
+++ app/Http/Controllers/Portal/DentalListController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Portal;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Region;
+use App\Models\Prefecture;
 use App\Models\Manage;
 use App\Models\MedicalParentCategory;
+use App\Models\MedicalChildrenCategory;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use App\Http\Controllers\CommonController;
@@ -16,13 +18,26 @@ class DentalListController extends Controller
     public function fetch(Request $request)
     {
         $dentals = Manage::query();
-        if ($request->has('number')) $dentals = $dentals->where('prefecture_number', $request['number']);
+        if ($request->has('prefecture')) {
+            if (!empty($request['prefecture'])) {
+                $dentals = $dentals->where('prefecture_number', $request['prefecture']);
+            }
+        }
+        if ($request->has('treat')) {
+            if (!empty($request['treat'])) {
+                $dentals = $dentals->withWhereHas('medical_treatments', function ($query) use ($request) {
+                    $query->whereIn('medical_children_category_id', $request['treat']);
+                });
+            }
+        }
         $dentals = $dentals->withWhereHas('basic_information', function ($query) {
             $query->whereNotNull('business_start')->whereNotNull('business_end');
         })->with('selected_station')->get();
         $regions = Region::with('prefectures')->get();
         $categories = MedicalParentCategory::with('children')->get();
-        $contents = ['regions' => $regions, 'dentals' => $dentals, 'categories' => $categories];
+        $children_categories = MedicalChildrenCategory::all();
+        $prefectures = Prefecture::all();
+        $contents = ['regions' => $regions, 'dentals' => $dentals, 'categories' => $categories, 'children_categories' => $children_categories, 'prefectures' => $prefectures];
         return response()->json($contents);
     }
 
