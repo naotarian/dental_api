6dd5197338b30c3f62d73179dc2fc462ef117440
naotarian <naotarian19@gmail.com>
2023-06-17 11:56:48 +0900

    ミニカレンダーデータ作成

--- app/Http/Controllers/Portal/DentalListController.php
+++ app/Http/Controllers/Portal/DentalListController.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\CommonController;
 
 class DentalListController extends Controller
 {
-    public function fetch(Request $request)
+    public function fetch(CommonController $common, Request $request)
     {
         $dentals = Manage::query();
         if ($request->has('prefecture')) {
@@ -32,7 +32,24 @@ class DentalListController extends Controller
         }
         $dentals = $dentals->withWhereHas('basic_information', function ($query) {
             $query->whereNotNull('business_start')->whereNotNull('business_end');
-        })->with('selected_station')->with('medical_treatments')->get();
+        })->with('selected_station')->with('medical_treatments')->get()->toArray();
+        //DentalCardに表示するミニカレンダー作成
+        //当日
+        $today = Carbon::today();
+        $end_date = $today->copy()->addWeeks(3);
+        $dates = CarbonPeriod::create($today, $end_date)->toArray();
+        foreach($dentals as $key => &$dental) {
+            $dental['calendarMin'] = [];
+            foreach($dates as $index => $date) {
+                $dental['calendarMin'][$index]['day'] = $date->format('Y/m/d');
+                $dental['calendarMin'][$index]['display_day'] = $date->format('n/j');
+                $dental['calendarMin'][$index]['dow_number'] = $date->dayOfWeekIso;
+                $dental['calendarMin'][$index]['dow'] = $date->isoFormat('ddd');
+                $dental['calendarMin'][$index]['is_holiday'] = $common->isHoliday($date);
+                $dental['calendarMin'][$index]['threshold'] = $date->dayOfWeekIso;
+                $dental['calendarMin'][$index]['today'] = $key === 0 ? true : false;
+            }
+        }
         $regions = Region::with('prefectures')->get();
         $categories = MedicalParentCategory::with('children')->get();
         $children_categories = MedicalChildrenCategory::all();
